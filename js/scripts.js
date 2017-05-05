d12e87f1ff15a9042f8f2177dd7dd1a1fa919778
YcleptInsan <nickmwise@gmail.com>
2017-05-05 13:56:45 -0700

    Change logic of backend to get remainder of var i instead of the user input.

--- js/scripts.js
+++ js/scripts.js
@@ -5,39 +5,38 @@ var array = function(number) {
 	var pingPongArray = [];
 	console.log(pingPongArray);
 
-		for(var i = 0; i <= number; i ++) {
+		for(var i = 0; i <= number; i++) {
 
 			// FOR NON POSITIVE NUMBERS DO NOTHING AND RETURN UNDEFINED
 				if (number > 0) {
 					// IF THE USERS INPUT IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY
-					if (number % 15 === 0) {
+					if ((i % 3 === 0) && (i % 5 === 0)) {
 						pingPongArray.push("Ping-Pong");
 						//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
-					} else if (number % 3 === 0) {
-
+					} else if (i % 3 === 0) {
 						pingPongArray.push("Ping");
 						//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
-					} else if (number % 5 === 0) {
-
+					} else if (i % 5 === 0) {
 						pingPongArray.push("Pong");
-
+					} else {
+						pingPongArray.push(i);
 					}
 
 				// NEW ARRAY THAT CHOOSES THE END OF THE ARRAY TO BE STORED IN pingPongArray
-				var result = pingPongArray.splice(-1,1);
+				var result = pingPongArray.splice(-1, 1);
 				console.log(result);
 				}
-				return result;
 			}
+			return result;
 		}
 
 // FRONT END LOGIC
 $(document).ready(function() {
 	$("form#ping_pong").submit(function(event){
 		event.preventDefault();
-			var number = $("input#number").val();
+			var userInput = parseInt($("input#number").val());
 
-			$("#result").append("<p>"+ array(number) +"</p>");
+			$("#result").append("<p>"+ array(userInput) +"</p>");
 				$("p").prev().remove();
 	});
 });
