6091941417f5f9ed6d85a456b45152d251da1174
YcleptInsan <nickmwise@gmail.com>
2017-05-05 10:41:17 -0700

    Print Ping-Pong in result window, but only prints that text. for loop or if statments not working properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 
 // BACKEND LOGIC
-var ping_pong = (function(userNumber){
+var ping_pong = function(userNumber) {
 
 	var pingPongArray = [];
-	console.log(pingPongArray);
+		console.log(pingPongArray);
 
-		for(var index = 0; index >= userNumber; index ++) {
+		for(var index = 0; index <= userNumber; index++) {
+			// FOR NON POSITIVE NUMBERS DO NOTHING AND RETURN UNDEFINED
+			if(userNumber > 0) {
+					// IF THE USERS INPUT IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY
+				if (index % 15 === 0) {
+					pingPongArray.push("Ping-Pong");
+					//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
+				} else if (index % 3 === 0){
+					pingPongArray.push("Ping");
+					//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
+				} else if (index % 5 === 0){
+					pingPongArray.push("Pong");
 
-			if (index % 3 === 0 && index % 5 === 0) {
-				pingPongArray.push("Ping-Pong");
-			} else if (index % 3 === 0){
-				pingPongArray.push("Ping");
-			} else if (index % 5 === 0){
-				pingPongArray.push("Pong");
 				}
+				// NEW ARRAY THAT CHOOSES THE END OF THE ARRAY TO BE STORED IN pingPongArray
+				var newArray = pingPongArray.splice(-1,1);
+				console.log(newArray);
+				}
+
+				return newArray
+
 			}
-			return pingPongArray ;
-		});
+		}
 
 // FRONT END LOGIC
 $(document).ready(function() {
@@ -25,7 +36,6 @@ $(document).ready(function() {
 			var userInput = parseInt($("input#number").val());
 
 
-
-			var result = $("#result").append("<p>"+ ping_pong(userInput) +"</p>");
+	$("#result").append("<p>"+ ping_pong(userInput) +"</p>");
 	});
 });
