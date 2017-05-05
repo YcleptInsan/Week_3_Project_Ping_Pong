716f8b773a6be8234519deb3d56b72e0f0923846
YcleptInsan <nickmwise@gmail.com>
2017-05-05 11:43:58 -0700

    Add notes in javascript. Get if else statements working properly. Loops through all statements. and returns array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,42 @@
 
 // BACKEND LOGIC
-var ping_pong = function(userNumber) {
+var array = function(number) {
 
 	var pingPongArray = [];
-		console.log(pingPongArray);
+	console.log(pingPongArray);
 
-		for(var index = 0; index <= userNumber; index++) {
+		for(var i = 0; i <= number; i++) {
 			// FOR NON POSITIVE NUMBERS DO NOTHING AND RETURN UNDEFINED
-			if(userNumber > 0) {
+				if (number > 0) {
 					// IF THE USERS INPUT IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY
-				if (index % 15 === 0) {
-					pingPongArray.push("Ping-Pong");
-					//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
-				} else if (index % 3 === 0){
-					pingPongArray.push("Ping");
-					//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
-				} else if (index % 5 === 0){
-					pingPongArray.push("Pong");
+					if (number % 15 === 0) {
+						pingPongArray.push("Ping-Pong");
+						//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
+					} else if (number % 3 === 0) {
 
-				}
+						pingPongArray.push("Ping");
+						//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
+					} else if (number % 5 === 0) {
+
+						pingPongArray.push("Pong");
+
+					}
+
 				// NEW ARRAY THAT CHOOSES THE END OF THE ARRAY TO BE STORED IN pingPongArray
-				var newArray = pingPongArray.splice(-1,1);
-				console.log(newArray);
+				// var result = pingPongArray.splice(-1);
+				// console.log(result);
 				}
-
-				return newArray
-
+				return pingPongArray;
 			}
 		}
 
 // FRONT END LOGIC
 $(document).ready(function() {
-	$("form#ping-pong").submit(function(event){
+	$("form#ping_pong").submit(function(event){
 		event.preventDefault();
 			var userInput = parseInt($("input#number").val());
 
 
-	$("#result").append("<p>"+ ping_pong(userInput) +"</p>");
+			$("#result").append("<p>"+ array(userInput) +"</p>");
 	});
 });
