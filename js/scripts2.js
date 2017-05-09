d91bc6d0e8c9ff844faced2d2d0287e860c1e292
YcleptInsan <nickmwise@gmail.com>
2017-05-09 16:57:41 -0700

    Add counting for loop. If statement not working properly.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,44 +1,47 @@
-
 // BACKEND LOGIC
 var array = function(number) {
 
 	var pingPongArray = [];
-	var count = 0
 	console.log(pingPongArray);
-	for(var i = 0; i <= number; i++) {
+	var count = 0;
+		for (var i = 0; i < number; i++){
 			count++;
 			pingPongArray.push(count);
+
 		}
-		for(var i = 0; i < pingPongArray.length; i++){
-			if (number > 0) {
+		for (var index = 0; index <= pingPongArray.length; index++) {
+
+			// FOR NON POSITIVE NUMBERS DO NOTHING AND RETURN UNDEFINED
+				if (number > 0) {
 					// IF THE USERS INPUT IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY
-			if (pingPongArray[i] % 15 === 0 ) {
+					if (index % 15 === 0 ) {
 						pingPongArray[i] = ("Ping-Pong");
 						//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
-				}else if (pingPongArray[i] % 3 === 0) {
+					} else if (index % 3 === 0) {
 						pingPongArray[i] = ("Ping");
 						//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
-				}else if (pingPongArray[i] % 5 === 0) {
+					} else if (index % 5 === 0) {
 						pingPongArray[i] = ("Pong");
-				}else {
-						pingPongArray = [i];
 					}
-				var result = pingPongArray.splice(-1,1);
+
+
+					// var result = pingPongArray[-1];
+					// console.log(result);
 				// NEW ARRAY THAT CHOOSES THE END OF THE ARRAY TO BE STORED IN variable result
-						}
-		return result;
+
+				}
+				return pingPongArray;
+			}
 		}
-}
 
 // FRONT END LOGIC
 $(document).ready(function() {
 	$("form#ping_pong").submit(function(event){
 		event.preventDefault();
 			var userInput = parseInt($("input#number").val());
-			var answerArray = array(userInput);
-				for(var i= 0; i< answerArray.length; i++) {
-			$("#result").append("<li>"+ answerArray[i] +"</li>");
-				$("li").prev().remove();
-			}
+			var output = array(userInput);
+			$("#result").append("<h4>"+ output +"</h4>");
+				$("h4").prev().remove();
+
 	});
 });
