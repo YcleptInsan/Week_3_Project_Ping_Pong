b76669664f208e0978a4420db49a9e7d8af320b9
YcleptInsan <nickmwise@gmail.com>
2017-05-09 17:26:21 -0700

    Change for loop to a forEach loop though if statements. decrmenting i by one so the string is palced in the proper index.

--- js/scripts2.js
+++ js/scripts2.js
@@ -4,35 +4,28 @@ var array = function(number) {
 	var pingPongArray = [];
 	console.log(pingPongArray);
 	var count = 0;
-		for (var i = 0; i < number; i++){
-			count++;
-			pingPongArray.push(count);
-
-		}
-		for (var index = 0; index <= pingPongArray.length; index++) {
-
+	// COUNTING FOR LOOP TO BE APPLIED TO PING PONG ARRAY
+	for (var i = 0; i < number; i++){
+		count++;
+		pingPongArray.push(count);
+	}
+		pingPongArray.forEach(function(i){
 			// FOR NON POSITIVE NUMBERS DO NOTHING AND RETURN UNDEFINED
-				if (number > 0) {
-					// IF THE USERS INPUT IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY
-					if (index % 15 === 0 ) {
-						pingPongArray[i] = ("Ping-Pong");
-						//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
-					} else if (index % 3 === 0) {
-						pingPongArray[i] = ("Ping");
-						//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
-					} else if (index % 5 === 0) {
-						pingPongArray[i] = ("Pong");
-					}
-
-
-					// var result = pingPongArray[-1];
-					// console.log(result);
-				// NEW ARRAY THAT CHOOSES THE END OF THE ARRAY TO BE STORED IN variable result
-
+				if (i > 0) {
+					// IF THE Index IS DIVISIBLE BY 15 PUSH PING-PONG INTO THE ARRAY at userInput val
+				if (i % 15 === 0 ) {
+					pingPongArray[i-1] = ("Ping-Pong");
+					//Else IF THE USERS INPUT IS DIVISIBLE BY 3 PUSH PING INTO THE ARRAY
+				} else if (i % 3 === 0) {
+					pingPongArray[i-1] = ("Ping");
+					//Else IF THE USERS INPUT IS DIVISIBLE BY 5 PUSH PONG INTO THE ARRAY
+				} else if (i % 5 === 0) {
+					pingPongArray[i-1] = ("Pong");
 				}
-				return pingPongArray;
 			}
-		}
+	});
+	return pingPongArray;
+}
 
 // FRONT END LOGIC
 $(document).ready(function() {
@@ -42,6 +35,5 @@ $(document).ready(function() {
 			var output = array(userInput);
 			$("#result").append("<h4>"+ output +"</h4>");
 				$("h4").prev().remove();
-
 	});
 });
